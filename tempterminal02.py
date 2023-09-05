2190c93bb935975b864420b799cfb9944977c824
AShura0001 <sajwanr@stu.otc.school.nz>
2023-09-06 09:28:01 +1200

    creating new game window in termpterminal_02

--- tempterminal02.py
+++ tempterminal02.py
@@ -29,6 +29,19 @@ def game_p2(window_size , main_file_directory):
     gwindow_2.fill(black)
     event_flag = True
 
+    card_placement_holder_1 = pygame.image.load(os.path.join(main_file_directory, "assets", "card_placement_holder.png")).convert_alpha()
+    card_placement_holder_1_rect = card_placement_holder_1.get_rect(center = (298, 250))
+    
+    card_placement_holder_2 = pygame.image.load(os.path.join(main_file_directory, "assets", "card_placement_holder.png")).convert_alpha()
+    card_placement_holder_2_rect = card_placement_holder_2.get_rect(center = (487, 250))
+
+
+    time_counter_bar_1 = pygame.image.load(os.path.join(main_file_directory, "assets", "time_counter_bar_1.png")).convert_alpha()
+    time_counter_bar_1_rect = time_counter_bar_1.get_rect(center = (380, 169))
+    
+    time_counter_bar_2 = pygame.image.load(os.path.join(main_file_directory, "assets", "time_counter_bar_2.png")).convert_alpha()
+    time_counter_bar_2_rect = time_counter_bar_2.get_rect(center = (380, 355))
+
     while event_flag:
         for event in pygame.event.get():
 
@@ -39,7 +52,12 @@ def game_p2(window_size , main_file_directory):
                 if event.key == pygame.K_ESCAPE:
                     event_flag = False
 
-        press_ESC_text = display_text(gwindow_2, "press ESC to exit", white, 29.55, 24.44, 27, "AGENCYR")
+        gwindow_2.blit(card_placement_holder_1, card_placement_holder_1_rect)
+        gwindow_2.blit(card_placement_holder_2, card_placement_holder_2_rect)
+        gwindow_2.blit(time_counter_bar_1, time_counter_bar_1_rect)
+        gwindow_2.blit(time_counter_bar_2, time_counter_bar_2_rect)
+
+        press_ESC_text = display_text(gwindow_2, "press ESC to exit", white, 19.55, 14.44, 17, "AGENCYR")
         pygame.display.update()
 
 game_p2(big_window_size, main_file_directory)
