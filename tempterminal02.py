c38dafb560424703239135e6ed6f505fbbe65abe
AShura0001 <sajwanr@stu.otc.school.nz>
2023-09-21 13:54:20 +1200

    Created a new function game_p2 (game panel 2) This function is the actual game feature current status of gp2 : INCOMPLETE Features to be added: 1. new cards 2. feature to move the scroll bars 3. display ranks 4. side menu bottom toggle buttons as in blueprint note: gp2 is not yet connected to the main menu

--- tempterminal02.py
+++ tempterminal02.py
@@ -33,6 +33,12 @@ def game_p2(window_size , main_file_directory):
     current_card_nos_2 = 26
 
     initial_time_sec = 10*60
+
+    vertical_scroll_bar_pos = 70
+    horizontal_scroll_bar_pos = 56
+
+
+
     card_placement_holder_1 = pygame.image.load(os.path.join(main_file_directory, "assets", "card_placement_holder.png")).convert_alpha()
     card_placement_holder_1_rect = card_placement_holder_1.get_rect(center = (298, 250))
     
@@ -45,6 +51,18 @@ def game_p2(window_size , main_file_directory):
     
     time_counter_bar_2 = pygame.image.load(os.path.join(main_file_directory, "assets", "time_counter_bar_2.png")).convert_alpha()
     time_counter_bar_2_rect = time_counter_bar_2.get_rect(center = (380, 355))
+
+    scroll_bar_vertical = pygame.image.load(os.path.join(main_file_directory, "assets", "scroll bar vertical GW2.png")).convert_alpha()
+    scroll_bar_vertical_rect = scroll_bar_vertical.get_rect(center = (950, 285))
+
+    scroll_bar_horizontal = pygame.image.load(os.path.join(main_file_directory, "assets", "scroll bar horizontal GW2.png")).convert_alpha()
+    scroll_bar_horizontal_rect = scroll_bar_horizontal.get_rect(center = (378, 548))
+
+    cards_title_GW2 = pygame.image.load(os.path.join(main_file_directory, "assets", "cards_title_GW2.png")).convert_alpha()
+    cards_title_GW2_rect = cards_title_GW2.get_rect(center = (858, 16))
+
+    GW2_side_menu_panel = pygame.image.load(os.path.join(main_file_directory, "assets", "GW2_side_menu_panel.png")).convert_alpha()
+    GW2_side_menu_panel_rect = GW2_side_menu_panel.get_rect(center = (858, 280))
 
     while event_flag:
         for event in pygame.event.get():
@@ -60,18 +78,28 @@ def game_p2(window_size , main_file_directory):
         gwindow_2.blit(card_placement_holder_2, card_placement_holder_2_rect)
         gwindow_2.blit(time_counter_bar_1, time_counter_bar_1_rect)
         gwindow_2.blit(time_counter_bar_2, time_counter_bar_2_rect)
+        gwindow_2.blit(scroll_bar_horizontal, scroll_bar_horizontal_rect)
+        horizontal_scroll_bar_indicator = pygame.draw.circle(gwindow_2, uni_red, (horizontal_scroll_bar_pos, 548), 5)
 
-        while initial_time_sec > 0:
-            initial_time_sec -= 1
-            time.sleep(1)
-            minutes = (initial_time_sec // 60)
-            seconds = (initial_time_sec % 60)
-            print (minutes,":",  seconds)
+
+        gwindow_2.blit(GW2_side_menu_panel, GW2_side_menu_panel_rect)
+        gwindow_2.blit(cards_title_GW2, cards_title_GW2_rect)
+        gwindow_2.blit(scroll_bar_vertical, scroll_bar_vertical_rect)
+        
+        vertical_scroll_bar_indicator = pygame.draw.circle(gwindow_2, uni_red, (950, vertical_scroll_bar_pos), 5)
+
 
         current_card_text_1 = display_text(gwindow_2, str(current_card_nos_1), white, 4, 155, 24, "OLDENGL")
         current_card_text_2 = display_text(gwindow_2, str(current_card_nos_2), white, 730, 342, 24, "OLDENGL")
         press_ESC_text = display_text(gwindow_2, "press ESC to exit", white, 19.55, 14.44, 17, "AGENCYR")
         pygame.display.update()
+        
+        # while initial_time_sec > 0:
+        #     initial_time_sec -= 1
+        #     time.sleep(1)
+        #     minutes = (initial_time_sec // 60)
+        #     seconds = (initial_time_sec % 60)
+        #     print (minutes,":",  seconds)
 
 game_p2(big_window_size, main_file_directory)
 pygame.quit()
