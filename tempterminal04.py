9e01f7427473a68196b2d1adf3716c9d6a33e575
Ashura <ashura.arc.organization@gmail.com>
2023-09-25 22:32:18 +1300

    Game_p2 Changes to be committed: 1. Added scrolling feature 2. Added functionability to arrow buttons 3. Added the new Game_p2 to the windows.py file

--- tempterminal04.py
+++ tempterminal04.py
@@ -1,11 +1,2 @@
-import time
-
-
-initial_time_sec = 2*60
-
-while initial_time_sec > 0:
-    initial_time_sec -= 1
-    time.sleep(1)
-    minutes = (initial_time_sec // 60)
-    seconds = (initial_time_sec % 60)
-    print (minutes,":",  seconds)
+import os
+print (os.get_cwd())
--- windows.py
+++ windows.py
@@ -17,6 +17,7 @@ def display_text(gwindow, text, color, x, y, font_size, Font):
     gwindow.blit(screen_text, [x,y])
     return screen_text_rect
 
+
 def support_window(window_size, main_file_directory):
     supp_window = pygame.display.set_mode(window_size, pygame.NOFRAME)
     event_flag = True
@@ -293,6 +294,8 @@ def game_p2(window_size , main_file_directory):
     gwindow_2 = pygame.display.set_mode(window_size)
     gwindow_2.fill(black)
     event_flag = True
+    vertical_indicator_flag = False
+    horizontal_indicator_flag = False
 
     current_card_nos_1 = 26
     current_card_nos_2 = 26
@@ -304,11 +307,19 @@ def game_p2(window_size , main_file_directory):
 
 
 
-    card_placement_holder_1 = pygame.image.load(os.path.join(main_file_directory, "assets", "card_placement_holder.png")).convert_alpha()
-    card_placement_holder_1_rect = card_placement_holder_1.get_rect(center = (298, 250))
+    card_placement_holder_1 = pygame.image.load(os.path.join(
+                    main_file_directory,"assets", "card_placement_holder.png")
+                                                ).convert_alpha()
+    card_placement_holder_1_rect = card_placement_holder_1.get_rect(
+                                                            center = (298, 250)
+                                                            )
     
-    card_placement_holder_2 = pygame.image.load(os.path.join(main_file_directory, "assets", "card_placement_holder.png")).convert_alpha()
-    card_placement_holder_2_rect = card_placement_holder_2.get_rect(center = (487, 250))
+    card_placement_holder_2 = pygame.image.load(os.path.join(
+                    main_file_directory, "assets", "card_placement_holder.png")
+                                                ).convert_alpha()
+    card_placement_holder_2_rect = card_placement_holder_2.get_rect(
+                                                            center = (487, 250)
+                                                            )
 
 
     time_counter_bar_1 = pygame.image.load(os.path.join(main_file_directory, "assets", "time_counter_bar_1.png")).convert_alpha()
@@ -319,9 +330,13 @@ def game_p2(window_size , main_file_directory):
 
     scroll_bar_vertical = pygame.image.load(os.path.join(main_file_directory, "assets", "scroll bar vertical GW2.png")).convert_alpha()
     scroll_bar_vertical_rect = scroll_bar_vertical.get_rect(center = (950, 285))
+    vertical_end_upper_rect = pygame.Rect((940 , 32), (20, 12))
+    vertical_end_lower_rect = pygame.Rect((940 , 523), (20, 12))
 
     scroll_bar_horizontal = pygame.image.load(os.path.join(main_file_directory, "assets", "scroll bar horizontal GW2.png")).convert_alpha()
     scroll_bar_horizontal_rect = scroll_bar_horizontal.get_rect(center = (378, 548))
+    horizontal_end_left_rect = pygame.Rect((0 , 538), (22, 20))
+    horizontal_end_right_rect = pygame.Rect((738 , 538), (22, 20))
 
     cards_title_GW2 = pygame.image.load(os.path.join(main_file_directory, "assets", "cards_title_GW2.png")).convert_alpha()
     cards_title_GW2_rect = cards_title_GW2.get_rect(center = (858, 16))
@@ -329,16 +344,11 @@ def game_p2(window_size , main_file_directory):
     GW2_side_menu_panel = pygame.image.load(os.path.join(main_file_directory, "assets", "GW2_side_menu_panel.png")).convert_alpha()
     GW2_side_menu_panel_rect = GW2_side_menu_panel.get_rect(center = (858, 280))
 
+
+    vertical_bar_markings = [(70 + (36*i)) for i in range(0, 13)]
+    horizontal_bar_markings = [(56 + (54*i)) for i in range(0, 13)]
+
     while event_flag:
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                event_flag = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    event_flag = False
-
         gwindow_2.blit(card_placement_holder_1, card_placement_holder_1_rect)
         gwindow_2.blit(card_placement_holder_2, card_placement_holder_2_rect)
         gwindow_2.blit(time_counter_bar_1, time_counter_bar_1_rect)
@@ -350,21 +360,74 @@ def game_p2(window_size , main_file_directory):
         gwindow_2.blit(GW2_side_menu_panel, GW2_side_menu_panel_rect)
         gwindow_2.blit(cards_title_GW2, cards_title_GW2_rect)
         gwindow_2.blit(scroll_bar_vertical, scroll_bar_vertical_rect)
-        
         vertical_scroll_bar_indicator = pygame.draw.circle(gwindow_2, uni_red, (950, vertical_scroll_bar_pos), 5)
+        for event in pygame.event.get():
 
+            if event.type == pygame.QUIT:
+                event_flag = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    event_flag = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # print (pygame.mouse.get_pos())
+                if scroll_bar_vertical_rect.collidepoint(event.pos):
+                    vertical_indicator_flag = True
+                    horizontal_indicator_flag = False
+                if scroll_bar_horizontal_rect.collidepoint(event.pos):
+                    horizontal_indicator_flag = True
+                    vertical_indicator_flag = False
+                if (vertical_end_upper_rect.collidepoint(event.pos) and vertical_scroll_bar_pos > 70):
+                    vertical_scroll_bar_pos -= 36
+                if vertical_end_lower_rect.collidepoint(event.pos) and vertical_scroll_bar_pos < 502:
+                    vertical_scroll_bar_pos += 36
+                if horizontal_end_left_rect.collidepoint(event.pos) and horizontal_scroll_bar_pos > 56:
+                    horizontal_scroll_bar_pos -= 54
+                if horizontal_end_right_rect.collidepoint(event.pos) and horizontal_scroll_bar_pos < 704:
+                    horizontal_scroll_bar_pos += 54
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                vertical_indicator_flag = False
+                horizontal_indicator_flag = False
+            
+            if vertical_indicator_flag:
+                mouse = pygame.mouse.get_pos()
+                if mouse[1] in vertical_bar_markings:
+                    vertical_scroll_bar_pos = mouse[1]
+                elif  event.type == pygame.MOUSEMOTION and event.rel[1] > 0:
+                    for marking in vertical_bar_markings[1:]:
+                        if mouse[1] < marking:
+                            vertical_scroll_bar_pos = marking - 36
+                            break
+                elif  event.type == pygame.MOUSEMOTION and event.rel[1] < 0:
+                    for marking in vertical_bar_markings[:-1:-1]:
+                        if mouse[1] > marking:
+                            vertical_scroll_bar_pos = marking + 36
+                            break
+            
+            if horizontal_indicator_flag:
+                mouse = pygame.mouse.get_pos()
+                if mouse[0] in horizontal_bar_markings:
+                    horizontal_scroll_bar_pos = mouse[0]
+                elif  event.type == pygame.MOUSEMOTION and event.rel[0] > 0:
+                    for marking in horizontal_bar_markings[1:]:
+                        if mouse[0] < marking:
+                            horizontal_scroll_bar_pos = marking - 54
+                            break
+                elif  event.type == pygame.MOUSEMOTION and event.rel[0] < 0:
+                    for marking in horizontal_bar_markings[:-1:-1]:
+                        if mouse[0] > marking:
+                            horizontal_scroll_bar_pos = marking + 54
+                            break
+            
+            
+                
 
         current_card_text_1 = display_text(gwindow_2, str(current_card_nos_1), white, 4, 155, 24, "OLDENGL")
         current_card_text_2 = display_text(gwindow_2, str(current_card_nos_2), white, 730, 342, 24, "OLDENGL")
         press_ESC_text = display_text(gwindow_2, "press ESC to exit", white, 19.55, 14.44, 17, "AGENCYR")
         pygame.display.update()
-        
-        # while initial_time_sec > 0:
-        #     initial_time_sec -= 1
-        #     time.sleep(1)
-        #     minutes = (initial_time_sec // 60)
-        #     seconds = (initial_time_sec % 60)
-        #     print (minutes,":",  seconds)
 
 def login_page(big_window_size, small_window_size, main_file_directory):
     gwindow = pygame.display.set_mode(big_window_size)
